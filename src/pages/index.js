ecc7f8fe9ab3011532146d8de1059980e5ca7b89
kristo.jeremy <kristo.jeremy@ui.ac.id>
2023-09-08 10:51:11 +0700

    fix: project structure

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function index() {
+  return (
+    //Navbar component will be imported here
+    <>
+      <div className="bg-black">RX-Labs Company Profile</div>
+    </>
+    //Footer component will be imported here
+  );
+}
+
+export default index;
