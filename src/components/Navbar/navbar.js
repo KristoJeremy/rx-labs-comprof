ef72b64e804538ef0a3b71a3eba6d5524b91f9f4
Naufal Rafi <naufalrafiar@gmail.com>
2023-09-21 00:37:05 +1000

    style: fix unresponsiveness on navbar and background

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -13,14 +13,14 @@ export const NavigationBar = () => {
   return (
     <section id="Navbar" className={styles.navbar}>
       <nav
-        className={`md:absolute py-5 transition-colors duration-500  ${
+        className={`md:absolute py-5 transition-colors duration-500 ${
           navbar
-            ? "background-color: transparent"
+            ? "background-color: transparent backdrop-blur-lg  "
             : "background-color: transparent"
         } transition-colors duration-500`}
       >
         <div className="container md:px-12 md:flex md:items-center md:justify-between lg:max-w-[1300px] md:mb-0 mb-[-5px]">
-          <div className="flex items-center justify-between md:mx-5">
+          <div className="flex items-center justify-between md:mx-5 px-4 ">
             <div
               className={`${styles.wrapper} lg:mx-5`}
               style={PoppinsBold.style}
