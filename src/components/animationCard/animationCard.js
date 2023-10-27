adb671059e6f5b294a829c92ca105a4b734288b9
Michael Baptiswa Marully <michael.baptiswa@ui.ac.id>
2023-10-27 15:27:00 +0700

    temporary

--- src/components/animationCard/animationCard.js
+++ src/components/animationCard/animationCard.js
@@ -53,33 +53,37 @@ export const AnimationCard = () => {
   return (
     <>
   <div className="flex justify-center">
-    <section className="w-2/3">
-      <div className="shadow-[3px_4px_4px_0px_rgba(64,_56,_69,_0.25)] bg-[rgba(66,_66,_70,_0.5)] flex flex-col justify-end gap-6 h-[700px] items-center py-5">
-        <div className="w-full aspect-w-16 aspect-h-9">
-          <Swiper
-            slidesPerView={"auto"}
-            centeredSlides={true}
-            spaceBetween={30}
-            autoplay={{
-              delay: 3500,
-              disableOnInteraction: false,
-            }}
-            modules={[Autoplay]}
-            className={`mySwiper`}
-          >
-            {Card()}
-          </Swiper>
-        </div>
-        <div className="text-center text-5xl font-['Poppins'] font-bold text-white">
-          Let's Begin Our Journey Together!
-        </div>
-        <div className="text-center text-xl font-['Lato'] text-[#c9a1b6] w-3/5">
-          Ready to embark on an exciting career journey with us? Join RX Labs and be
-          a part of something extraordinary. Your future starts here!
+    <section className="flex justify-center">
+      <div className="flex flex-col w-2/3 justify-center relative">
+        <div className="shadow-[3px_4px_4px_0px_rgba(64,_56,_69,_0.25)] bg-[rgba(66,_66,_70,_0.5)] flex flex-col justify-end gap-6 h-[700px] items-center py-5">
+          <div className="w-full aspect-w-16 aspect-h-9">
+            <Swiper
+              slidesPerView={"auto"}
+              centeredSlides={true}
+              spaceBetween={30}
+              autoplay={{
+                delay: 3500,
+                disableOnInteraction: false,
+              }}
+              modules={[Autoplay]}
+              className={`mySwiper absolute inset-0 overflow-visible`}
+              style={{ zIndex: 0 }}
+            >
+              {Card()}
+            </Swiper>
+          </div>
+          <div className="text-center text-5xl font-['Poppins'] font-bold text-white">
+            Let's Begin Our Journey Together!
+          </div>
+          <div className="text-center text-xl font-['Lato'] text-[#c9a1b6] w-3/5">
+            Ready to embark on an exciting career journey with us? Join RX Labs and be
+            a part of something extraordinary. Your future starts here!
+          </div>
         </div>
       </div>
     </section>
   </div>
 </>
+
   );
 };
